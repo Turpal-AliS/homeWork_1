420523899d64cb23bf6a500089b1acbe6d7482b2
Turpal-Ali <turpalalih61@gmail.com>
2023-03-06 00:41:27 +0300

    add home work

---
+++ numbers.cs
@@ -0,0 +1,112 @@
+﻿/*
+// 1
+
+float num_1 = 3.14f;
+float num_2 = 42.0f;
+double sum = num_1 + num_2;
+Console.WriteLine(sum);
+ */
+
+/*
+// 2
+
+int numberOne = 33;
+int numberTwo = 22;
+int result = numberOne / numberTwo;
+int remainder = numberOne % numberTwo;
+Console.WriteLine($"При делении {numberOne} на {numberTwo} результат равен {result}, остаток равен {remainder}");
+ */
+
+/*
+// 3.1
+
+static void funcSign(int n)
+{
+    int fixNum = n * (-1);
+    Console.WriteLine(fixNum);
+}
+
+Console.Write("Введите отрицательное число: ");
+int i = int.Parse(Console.ReadLine());
+funcSign(i);
+ */
+
+/*
+// 3.2
+
+static void func(int n)
+{
+    int sum = 0;
+    if(n > 0)
+    {
+        for (int i = 0; i < n; n--)
+        {
+            sum += n;
+        }
+        Console.WriteLine(sum);
+    }
+    else
+    {
+        for (int i = 0; i > n; n++)
+        {
+            sum += n;
+        }
+        Console.WriteLine(sum);
+    }
+}
+Console.Write("Введите число: ");
+int i = int.Parse(Console.ReadLine());
+
+func(x);
+*/
+
+/*
+
+// 4
+
+int month = 30;
+int yearD = 360;
+int yearM = 12;
+int secondsInday = 86400;
+
+Console.WriteLine("Введите пошагово день рождения, месяц рождения и год рождения");
+int birthDay = int.Parse(Console.ReadLine());
+int birthMonth = int.Parse(Console.ReadLine());
+int birthYear = int.Parse(Console.ReadLine());
+
+int today = 28;
+int currentMonth = 2;
+int currentYear = 2023;
+
+int ageYear = (((currentYear * yearM - birthYear * yearM) - birthMonth + currentMonth) / yearM);
+int ageMonth = (((((currentYear * yearM - birthYear * yearM) - birthMonth + currentMonth) % yearM) * month - birthDay + today)) / month;
+int ageDay = (((((currentYear * yearM - birthYear * yearM) - birthMonth + currentMonth) % yearM) * month - birthDay + today)) % month;
+
+// 4.2
+
+Console.WriteLine($"{ageYear} years, {ageYear * yearM + ageMonth} month, {ageYear * yearD + ageMonth * month + ageDay} days and {ageYear * yearD * secondsInday + ageMonth * month * secondsInday + ageDay * secondsInday} seconds have passed since my birth");
+
+// 4.3
+
+if (birthMonth > 0 && birthMonth <= 3)
+{
+    Console.WriteLine("Вы родились в 1-м квартале");
+}
+else if (birthMonth > 3 && birthMonth <= 6)
+{
+    Console.WriteLine("Вы родились в 2-м квартале");
+}
+else if (birthMonth > 6 && birthMonth <= 9)
+{
+    Console.WriteLine("Вы родились в 3-м квартале");
+}
+else if (birthMonth > 9 && birthMonth <= 12)
+{
+    Console.WriteLine("Вы родились в 4-м квартале");
+}
+
+else
+{
+    Console.WriteLine("Возможно, вы ввели данный не корректно!");
+}
+ */
---
+++ strings.cs
@@ -0,0 +1,148 @@
+﻿/*
+// 1.1 
+
+string word = "hello";
+char firstLetter = char.ToUpper(word[0]);
+word = word.Replace(word[0], firstLetter);
+Console.WriteLine(word);
+ */
+
+/*
+// 1.2
+
+string str = "This morning I get up at a half past six o’clock. It’s still dark. I feel tired.";
+int counter = 0;
+for(int i = 0; i < str.Length; i++)
+{
+    if(str[i] == ' ')
+    {
+        counter++;
+    }
+}
+Console.WriteLine(counter);
+ */
+
+// 2
+
+//string history = "History is always written by the winners! When two cultures clash, the loser is obliterated, and the winner writes the history books - books which glorify their own cause and disparage the conquered foe. As Napoleon once said, 'What is history, but a fable agreed upon?";
+
+/*
+// 2.2
+
+string[] words = history.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+int counter = 0;
+
+foreach (string s in words)
+{
+    counter++;
+}
+
+Console.WriteLine(counter);
+*/
+
+/*
+// 2.3
+
+Console.WriteLine(history.Length);
+*/
+
+/*
+// 2.4
+
+int counter = 0;
+
+for(int i = 0; i < history.Length; i++)
+{
+    counter++;
+
+    if(history[i] == ' ')
+    {
+        counter--;
+    }
+}
+
+Console.WriteLine(counter);
+*/
+
+/*
+// 3
+
+string upLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+string lowLetter = "abcdefghijklmnopqrstuvwxyz";
+string str = "Aa Bb cc";
+int upCounter = 0;
+int lowCounter = 0;
+for(int i = 0; i < str.Length; i++)
+{
+    for(int j = 0; j < upLetter.Length; j++)
+    {
+        if (str[i] == upLetter[j])
+        {
+            upCounter++;
+        }
+        else if(str[i] == lowLetter[j])
+        {
+            lowCounter++;
+        }
+    }
+}
+Console.WriteLine(upCounter);
+Console.WriteLine(lowCounter); 
+*/
+
+/*
+// 4
+
+static void Palindrome(string input)
+{
+    input = input.ToLower().Replace(" ", "");
+
+    for(int i = 0; i < input.Length / 2; i++)
+    {
+        if(input[i] != input[input.Length - i -1])
+        {
+            Console.WriteLine("Данная строка или слова не яв-ся палиндромом");
+            return;
+        }
+    }
+
+    Console.WriteLine("Данная строка или слова яв-ся палиндромом");
+}
+
+Palindrome("А роза упала на лапу Азора"); 
+*/
+
+/*
+// 5
+
+static void FuncCase(string str)
+{
+    char[] chars = str.ToCharArray();
+
+    for(int i = 0; i < chars.Length; i++)
+    {
+        if(!Char.IsLetter(chars[i]))
+        {
+            continue;
+        }
+
+        if (i % 2 == 0)
+        {
+            chars[i] = Char.ToUpper(chars[i]);
+        }
+        else
+        {
+            chars[i] = Char.ToLower(chars[i]);
+        }
+    }
+
+    string outStr = "";
+    for (int i = 0; i < chars.Length; ++i)
+    {
+        outStr += chars[i];
+    }
+    Console.WriteLine(outStr);
+}
+
+FuncCase("Привет, Мир!"); 
+*/
